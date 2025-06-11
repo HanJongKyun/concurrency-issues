28dadc7f91278f750ae60c1d45489565bdb92ca8
Jongkyun <hjk326333@gmail.com>
2025-06-11 10:20:56 +0900

    동시성 이슈 템플릿 구성

---
+++ src/main/java/com/playdata/concurrencyissues/entity/Stock.java
@@ -0,0 +1,26 @@
+package com.playdata.concurrencyissues.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Stock {
+
+    @Id
+    private Long id;
+
+    private Long productId;
+    private Long quantity;
+
+    public void decreaseQuantity(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("재고는 0 미만이 될 수 없어요!");
+        }
+        this.quantity -= quantity;
+    }
+
+}
